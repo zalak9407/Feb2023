7f626461b3d806812ba5b2c67f969b2b60501373
zalak patel <jeelpatel508@gmail.com>
2023-02-28 00:41:59 +1300

    Edit and del record test

--- Feb2023/Feb2023/Program.cs
+++ Feb2023/Feb2023/Program.cs
@@ -1,9 +1,9 @@
 ﻿// open cHrome browser
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Events;
 
-
-        IWebDriver driver = new ChromeDriver();
+IWebDriver driver = new ChromeDriver();
         driver.Manage().Window.Maximize();
 
 
@@ -76,8 +76,8 @@ IWebElement gotolast = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/
 gotolast.Click();
 Thread.Sleep(5000);
 
-IWebElement newcode = driver.FindElement(By.XPath("//*[@id=\" tmsGrid \"]/div[3]/table/tbody/tr[last()]/td[1]"));
-
+IWebElement newcode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+//Thread.Sleep(1000);
 if(newcode.Text == "feb2023")
 {
     Console.WriteLine("Recored created successfully");
@@ -86,4 +86,91 @@ else
 {
     Console.WriteLine("Not added");
 }
+//click on edit button to edit record
 
+IWebElement editbtn = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+editbtn.Click();
+
+//clear record from code textbox
+IWebElement clrcode = driver.FindElement(By.Id("Code"));
+clrcode.Clear();
+IWebElement edttextbox = driver.FindElement(By.Id("Code"));
+edttextbox.SendKeys("march2023");
+
+//clear record from description textbox
+IWebElement clrdescription = driver.FindElement(By.Id("Description"));
+clrdescription.Clear();
+IWebElement detextbox = driver.FindElement(By.Id("Description"));
+detextbox.SendKeys("june2023");
+
+//clear record from priceperunit textbox
+//driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]")).Clear();
+//Thread.Sleep(2000);
+
+//driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]")).SendKeys("13");
+////*[@id="TimeMaterialEditForm"]/div/div[4]/div/span[1]/span/input[1]
+//*[@id="TimeMaterialEditForm"]/div/div[4]/div/span[1]/span
+//*
+//*[@id="TimeMaterialEditForm"]/div/div[4]/div/span[1]/span
+////*[@id="Price"]
+
+//IWebElement pputextbox = driver.FindElement(By.Id("Price"));
+
+
+//Click on save button to edit the record
+IWebElement savebtn = driver.FindElement(By.Id("SaveButton"));
+savebtn.Click();
+Thread.Sleep(2000);
+
+//gotolats page to check record edited successfully
+IWebElement lastbtnclick = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+lastbtnclick.Click();
+Thread.Sleep(3000);
+
+IWebElement editedrecored = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if(editedrecored.Text == "march2023")
+{
+    Console.WriteLine("Record in code textbox edited successfully");
+
+}
+else
+{
+    Console.WriteLine("record not edited");
+
+}
+
+//check record edited successfully in description textbox
+IWebElement editde = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[3]"));
+//*[@id="tmsGrid"]/div[3]/table/tbody/tr[3]/td[3]
+if (editde.Text == "june2023")
+{
+    Console.WriteLine("Record in description textbox edited successfully");
+
+}
+else
+{
+    Console.WriteLine("Record in description textbox not edited");
+}
+
+
+//delete record
+IWebElement delrecord = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+delrecord.Click();
+Thread.Sleep(1000);
+
+// Click on ok button
+IAlert al = driver.SwitchTo().Alert();
+al.Accept();
+Thread.Sleep(1000);
+
+IWebElement delre = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[3]"));
+if(delre.Text== "june2023")
+{
+    Console.WriteLine("Record not deleted successfully");
+}
+else
+{
+    Console.WriteLine("Record deleted successfully");
+}
+driver.Quit();
